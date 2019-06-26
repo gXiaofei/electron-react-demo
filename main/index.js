31451aac55fed66f94641246b336bdcb0985cacf
gxf <310877593.com>
2019-06-26 10:18:08 +0800

    update

--- main/index.js
+++ main/index.js
@@ -7,9 +7,9 @@ import handleMessage from './event/message';
 import onCrashed from './protect/crashed';
 import createTray from './protect/tray';
 import autoStart from './protect/autoStart';
-
-
-const { app, BrowserWindow } = require('electron');
+// import {updateHandle, sendUpdateMessage} from './autoUpdate/update'
+import { autoUpdater } from "electron-updater"
+const { app, BrowserWindow, ipcMain} = require('electron');
 
 let mainWindow;
 
@@ -27,6 +27,9 @@ function createWindow() {
     });
   }
   global.mainId = mainWindow.id;
+
+  // 尝试更新
+  // updateHandle(mainWindow);
 }
 
 if (process.platform === 'win32') {
@@ -58,3 +61,51 @@ app.on('activate', function () {
     createWindow()
   }
 });
+
+
+// 主进程监听渲染进程传来的信息
+ipcMain.on('update', (e, arg) => {
+  console.log("update");
+  checkForUpdates();
+});
+let feedUrl = 'http://127.0.0.1:8080/';
+let checkForUpdates = () => {
+  // 配置安装包远端服务器
+  autoUpdater.setFeedURL(feedUrl);
+
+  // 下面是自动更新的整个生命周期所发生的事件
+  autoUpdater.on('error', function(message, a, b) {
+    console.log(message, a, b);
+      sendUpdateMessage('error', message);
+  });
+  autoUpdater.on('checking-for-update', function(message) {
+      sendUpdateMessage('checking-for-update', message);
+  });
+  autoUpdater.on('update-available', function(message) {
+      sendUpdateMessage('update-available', message);
+  });
+  autoUpdater.on('update-not-available', function(message) {
+      sendUpdateMessage('update-not-available', message);
+  });
+
+  // 更新下载进度事件
+  autoUpdater.on('download-progress', function(progressObj) {
+      sendUpdateMessage('downloadProgress', progressObj);
+  });
+  // 更新下载完成事件
+  autoUpdater.on('update-downloaded', function(event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+      sendUpdateMessage('isUpdateNow');
+      ipcMain.on('updateNow', (e, arg) => {
+          autoUpdater.quitAndInstall();
+      });
+  });
+
+  //执行自动更新检查
+  autoUpdater.checkForUpdates();
+};
+
+// 主进程主动发送消息给渲染进程函数
+function sendUpdateMessage(message, data) {
+  console.log({ message, data });
+  mainWindow.webContents.send('message', { message, data });
+}
--- render/views/Home/index.js
+++ render/views/Home/index.js
@@ -3,17 +3,67 @@ import { Layout } from 'antd';
 import { observer } from 'mobx-react';
 import store from './store';
 import './index.css';
-
+import { ipcRenderer } from "electron";
 const { Content } = Layout;
 
 @observer
 class Home extends React.Component {
 
+
+  state = {
+    downloadPercent: 0,
+    tips: ''
+  }
   componentDidMount() {
     store.initValue();
+
+    // ipcRenderer.send("checkForUpdate");
+
+
+    // ipcRenderer.on("message", (event, text) => {
+    //   console.log(arguments);
+    //   this.tips = text;
+    // });
+    // //注意：“downloadProgress”事件可能存在无法触发的问题，只需要限制一下下载网速就好了
+    // ipcRenderer.on("downloadProgress", (event, progressObj)=> {
+    //     console.log(progressObj);
+    //     this.downloadPercent = progressObj.percent || 0;
+    // });
+    // ipcRenderer.on("isUpdateNow", () => {
+    //     ipcRenderer.send("isUpdateNow");
+    // });
+
+    ipcRenderer.on('message', (event, {message, data}) => {
+      console.log('message:', message);
+      console.log('data:', data);
+      if(message === 'isUpdateNow'){
+        if(confirm('是否现在更新？')){
+          ipcRenderer.send('updateNow');
+        }
+      }
+    })
   }
 
 
+
+  // import { ipcRenderer } from 'electron';
+  //  const { ipcRenderer } = require('electron'); 
+  //  export default { 
+  //    name: 'my-project1', 
+  //  mounted() { 
+  //    var _ol = document.getElementById("content"); 
+  //    ipcRenderer.on('message',(event,{message,data}) => { 
+  //      let _li = document.createElement("li"); 
+  //      _li.innerHTML = message + " <br>data:" + JSON.stringify(data) +"<hr>";
+  //       _ol.appendChild(_li); if (message === 'isUpdateNow') {
+  //          if (confirm('是否现在更新？')) { 
+  //            ipcRenderer.send('updateNow'); } } }); 
+  //           }, methods: { autoUpdate() { ipcRenderer.send('update'); } } 
+  //         };
+
+  autoUpdate = () => {
+    ipcRenderer.send('update');
+  }
   render() {
     return (
       <Content className="content">
@@ -21,6 +71,9 @@ class Home extends React.Component {
           <h2>第一页</h2>
           <h2>当前环境：{store.currentEnv}</h2>
           <h2>接口前缀：{store.currentUrlPrefix}</h2>
+          <div onClick={this.autoUpdate}>
+            更新
+          </div>
         </div>
       </Content>
     );
